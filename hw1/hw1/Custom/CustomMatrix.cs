d118065b2ebb12bec96c1db7e80f0c996ca04a06
junjun1023 <44397290+junjun1023@users.noreply.github.com>
2020-11-09 18:44:35 +0800

    Add extra links to implement numeric generic type

--- hw1/hw1/Custom/CustomMatrix.cs
+++ hw1/hw1/Custom/CustomMatrix.cs
@@ -4,6 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/// <summary>
+/// Another class
+/// https://stackoverflow.com/questions/1348594/is-there-a-c-sharp-generic-constraint-for-real-number-types
+/// </summary>
+/// TODO:
+
+
 namespace hw1.Custom
 {
     public class CustomMatrix
@@ -11,7 +18,7 @@ namespace hw1.Custom
         public CustomMatrix() { } // default Constructor
 
         public T[,] MatrixMultiply<T>(T[,] arr1, T[,] arr2) where T : struct, 
-                                                                        System.IComparable,
+                                                                        IComparable,
                                                                         IComparable<T>,
                                                                         IConvertible,
                                                                         IEquatable<T>,
@@ -42,6 +49,41 @@ namespace hw1.Custom
             }
 
 
+
+            return ret;
+        }
+
+
+        public T[,] MatrixTranspose<T>(T[,] arr1, T[,] arr2) where T : struct,
+            IComparable,
+            IComparable<T>,
+            IConvertible,
+            IEquatable<T>,
+            IFormattable
+        {
+            if (arr1.GetLength(1) != arr2.GetLength(0))
+            {
+                throw new System.ArgumentException("Dimension of arr2 cannot match to arr1", "arr2");
+            }
+
+            T[,] ret = new T[arr1.GetLength(0), arr2.GetLength(1)];
+            for (var i = 0; i < arr1.GetLength(0); i++)
+            {
+
+                for (var j = 0; j < arr2.GetLength(1); j++)
+                {
+                    dynamic sum = 0;
+                    for (var k = 0; k < arr1.GetLength(1); k++)
+                    {
+                        dynamic val1 = arr1[i, k], val2 = arr2[k, j];
+                        sum += val1 * val2;
+                    }
+
+                    ret[i, j] = sum;
+
+                }
+
+            }
 
             return ret;
         }
